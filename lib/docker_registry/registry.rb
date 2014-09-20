d7b88402998747de2aa8b7ecc5af88bcdf36f312
dan <daan.forever@gmail.com>
2014-09-20 13:40:54 +0400

    base_uri renamed to url

--- lib/docker_registry/registry.rb
+++ lib/docker_registry/registry.rb
@@ -5,12 +5,12 @@ require 'docker_registry/repository'
 require 'docker_registry/tag'
 
 class DockerRegistry::Registry
-  attr_reader :base_uri, :options, :client
+  attr_reader :url, :options, :client
 
   # @see DockerRegistry::Client#initialize
-  def initialize(uri)
-    @base_uri = uri
-    @uri = URI.parse(uri)
+  def initialize(url)
+    @url = url
+    @uri = URI.parse(url)
     @client = DockerRegistry::Client.new(
       "#{@uri.scheme}://#{@uri.host}:#{@uri.port}",
       user: @uri.user,
